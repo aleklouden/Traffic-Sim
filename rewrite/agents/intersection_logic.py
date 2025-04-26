570a717031826b9d36256d30a87c3961314083bf
aleklouden <aleklouden@gmail.com>
2025-04-26 13:19:42 -0500

    Update intersection_logic.py

---
+++ rewrite/agents/intersection_logic.py
@@ -0,0 +1,15 @@
+from infrastructure.intersection import Intersection
+
+def intersection_logic(type):
+    if type == "light":
+        pass
+
+
+def traffic_light():
+    pass
+
+def stop_sign():
+    pass
+
+def round_about():
+    pass
